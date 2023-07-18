a8c51d94a34e07200f5a7fe29563b5c57fdf86b4
Renata Alves <100436812+renataalvescun@users.noreply.github.com>
2023-07-17 22:36:47 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,403 @@
+var protagonista = [
+  "um estudante",
+  "um professor(a)",
+  "uma professora",
+  "um órfão(a)",
+  "uma fada",
+  "um investigador(a)",
+  "um bruxo(a)",
+  "uma heróina",
+  "um herói",
+  "um vilă/vilão",
+  "um criminoso(a)",
+  "um campones(a)",
+  "um mestre",
+  "um aprendiz",
+  "um cantor(a)",
+  "um membro de banda",
+  "um cientista",
+  "uma criança",
+  "uma mãe",
+  "um pai",
+  "um rei",
+  "uma rainha",
+  "uma sereia",
+  "um elfo",
+  "um demônio",
+  "um anjo",
+  "um vampiro",
+  "um policial",
+  "um espião(a)",
+  "um deus",
+  "um secretário(a)",
+  "um bastardo(a)",
+  "um mafioso(a)",
+  "um espadachim",
+  "um stalker",
+  "um adolescente",
+  "um meio-humano(a)",
+  "um romântico(a)",
+  "um vingativo(a)",
+  "um paranormal",
+  "um jornalista",
+  "um mágico(a)",
+  "um naufrágo(a)",
+  "um pirata",
+  "um guardião",
+  "um cientista",
+  "um imperador(triz)",
+  "uma aventureira",
+  "um comandante",
+  "um ET",
+  "um aniversariante",
+  "um líder de torcida",
+  "um pior aluno(a)",
+  "um melhor aluno(a)",
+  "um rico(a)",
+  "um rebelde",
+  "um demônio",
+  "um não escolhido",
+  "um sobrevivente",
+  "um nerd",
+  "um popular",
+  "um jogador(a)",
+  "um idol",
+  "um coveiro(a)",
+  "um escolhido",
+  "um caçador(a)",
+  "um zumbi",
+  "um famoso(a)",
+  "um azarado(a)",
+  "uma assassina",
+  "uma bailarina(o)",
+  "uma sereia",
+  "um fantasma",
+  "uma feiticeira(o)",
+  "um filho(a) de Deus",
+  "um tímido(a)",
+  "um imortal",
+  "um espião(a)",
+  "um escritor(a)",
+  "os gêmeos",
+  "um príncipe",
+  "uma princesa"
+  ];
+var protagonista2 = [
+    "estudante",
+    "professor(a)",
+    "professora",
+    "órfão(a)",
+    "fada",
+    "investigador(a)",
+    "bruxo(a)",
+    "heróina",
+    "herói",
+    "vilă/vilão",
+    "criminoso(a)",
+    "campones(a)",
+    "mestre",
+    "aprendiz",
+    "cantor(a)",
+    "membro de banda",
+    "cientista",
+    "criança",
+    "mãe",
+    "pai",
+    "rei",
+    "rainha",
+    "sereia",
+    "elfo",
+    "demônio",
+    "anjo",
+    "vampiro",
+    "policial",
+    "espião(a)",
+    "deus",
+    "secretário(a)",
+    "bastardo(a)",
+    "mafioso(a)",
+    "espadachim",
+    "stalker",
+    "adolescente",
+    "meio-humano(a)",
+    "romântico(a)",
+    "vingativo(a)",
+    "paranormal",
+    "jornalista",
+    "mágico(a)",
+    "naufrágo(a)",
+    "pirata",
+    "guardião",
+    "cientista",
+    "imperador(triz)",
+    "aventureira",
+    "comandante",
+    "ET",
+    "aniversariante",
+    "líder de torcida",
+    "pior aluno(a)",
+    "melhor aluno(a)",
+    "rico(a)",
+    "rebelde",
+    "demônio",
+    "não escolhido",
+    "sobrevivente",
+    "nerd",
+    "popular",
+    "jogador(a)",
+    "idol",
+    "coveiro(a)",
+    "escolhido",
+    "caçador(a)",
+    "zumbi",
+    "famoso(a)",
+    "azarado(a)",
+    "assassina",
+    "bailarina(o)",
+    "sereia",
+    "fantasma",
+    "feiticeira(o)",
+    "filho(a) de Deus",
+    "tímido(a)",
+    "imortal",
+    "espião(a)",
+    "escritor(a)",
+    "gêmeos",
+    "príncipe",
+    "princesa"
+  ];
+var plot = [
+    "viagem no tempo",
+    "escola",
+    "amnésia",
+    "assassinato",
+    "guerra",
+    "opostos",
+    "ilha perdida",
+    "diário",
+    "stalker",
+    "mafia",
+    "a profecia",
+    "o escolhido",
+    "clãs inimigos",
+    "um reino na ruína",
+    "nerd e popular",
+    "superação",
+    "clichê invertido",
+    "uma fuga",
+    "jornada de vilão",
+    "jornada de herói",
+    "vingança",
+    "amor proibido",
+    "conflito familiar",
+    "herdeiro perdido",
+    "artefato mágico",
+    "abrir portal",
+    "o que é amor?",
+    "vizinho novo",
+    "amigos de infância",
+    "maldição",
+    "rivalidade",
+    "vidas passadas",
+    "magia",
+    "serial killer",
+    "badboy e mocinho",
+    "traição",
+    "usurpação",
+    "troca de identidade",
+    "piratas",
+    "portal paranormal",
+    "casa assombrada",
+    "enemies to lovers",
+    "uma vida eterna",
+    "conto de fada",
+    "clube vs clube",
+    "meninas vs meninos",
+    "casamento contratual",
+    "trisal",
+    "triângulo amoroso",
+    "lobisomen",
+    "amigo imaginário",
+    "invasão zumbi",
+    "fim do mundo",
+    "vizinho estranho",
+    "troca de corpo",
+    "ler mentes",
+    "de repente magia",
+    "quem matou x?",
+    "famílias rivais",
+    "ajudando o cupido",
+    "viagem ao mundo",
+    "casamento arranjado",
+    "chantagem",
+    "troca de corpo",
+    "lovers to enemies",
+    "guerra",
+    "resgate",
+    "tentação",
+    "líder de seita",
+    "escola assombrada",
+    "sequestro errado",
+    "sequestro",
+    "harém",
+    "inimigos de infância",
+    "amigos de infância",
+    "salvar o mundo",
+    "visitando o inferno",
+    "concurso",
+    "errar remetente de SMS",
+    "baile fantasia"
+    ];
+var estilo = [
+      "fairycore",
+      "fairypunk",
+      "conto de fadas sombrio",
+      "high fantasy",
+      "realismo mágico",
+      "mágica urbana",
+      "steampunk",
+      "ficção mítica",
+      "fantasia contemporânea",
+      "piratapunk",
+      "dieselpunk",
+      "solarpunk",
+      "biopunk",
+      "cottagecore",
+      "cyberpunk",
+      "atual",
+      "medieval",
+      "asia feudal",
+      "distopia",
+      "utopia",
+      "anos 90",
+      "anos 50",
+      "egito antigo",
+      "vikings",
+      "futuristico",
+      "era ficticia",
+      "victoriana",
+      "moderno",
+      "moderno",
+      "moderno",
+      "moderno",
+      "moderno",
+      "moderno",
+      "moderno",
+      "moderno",
+      "moderno",
+      "moderno"
+    ];    
+var genero = [
+      "romance",
+      "mistério",
+      "fantasia",
+      "ficção científica",
+      "ficção adolescente",
+      "suspense",
+      "paranormal",
+      "ação",
+      "aventura",
+      "drama",
+      "horror",
+      "thriller psicológico",
+      "comédia",
+      "histórico",
+      "policial",
+      "conto de fadas",
+      "jovem adulto",
+      "distopia",
+      "histórico",
+      "utopia"
+      ];
+      
+function formarFrase() {
+  var sorteioProtagonista = protagonista[Math.floor(Math.random() * protagonista.length)];
+  var sorteioProtagonista2 = protagonista2[Math.floor(Math.random() * protagonista2.length)];
+  var sorteioPlot = plot[Math.floor(Math.random() * plot.length)];
+  var sorteioEstilo = estilo[Math.floor(Math.random() * estilo.length)];
+  var sorteioGenero = genero[Math.floor(Math.random() * genero.length)];
+
+  var fraseCompleta = "Seu personagem principal é " + sorteioProtagonista + "  " + sorteioProtagonista2 + ". Com o plot de " 
+  + sorteioPlot + ", no estilo  " + sorteioEstilo + " e do gênero " + sorteioGenero + ".";
+  document.getElementById("fraseCompleta").textContent = fraseCompleta;
+}
+
+function copiarFrase() {
+  var fraseCompleta = document.getElementById("fraseCompleta").textContent;
+  navigator.clipboard.writeText(fraseCompleta).then(function() {
+    alert("A frase foi copiada com sucesso!");
+  },  function() {
+    alert("Não foi possível copiar a frase.");
+  });
+}
+function refazerSorteio() {
+}
+
+// CADASTRO E LOGIN
+
+// Array para armazenar os usuários cadastrados
+var users = [];
+
+// Função para cadastrar um novo usuário
+function registerUser(event) {
+  event.preventDefault(); // Evita o comportamento padrão do formulário
+
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  // Verifica se o email já está cadastrado
+  if (getUserByEmail(email)) {
+    showMessage('Email já cadastrado');
+    return;
+  }
+
+  // Cria um objeto de usuário
+  var user = {
+    name: name,
+    email: email,
+    password: password
+  };
+
+  // Adiciona o usuário ao array de usuários
+  users.push(user);
+  showMessage('Cadastro realizado com sucesso!');
+}
+
+// Função para autenticar o login do usuário
+function loginUser(event) {
+  event.preventDefault(); // Evita o comportamento padrão do formulário
+
+  var email = document.getElementById('loginEmail').value;
+  var password = document.getElementById('loginPassword').value;
+
+  // Verifica se o usuário existe
+  var user = getUserByEmail(email);
+  if (!user) {
+    showMessage('Usuário não encontrado');
+    return;
+  }
+
+  // Verifica se a senha está correta
+  if (user.password !== password) {
+    showMessage('Senha incorreta');
+    return;
+  }
+
+  showMessage('Login realizado com sucesso!');
+}
+
+// Função para obter um usuário pelo email
+function getUserByEmail(email) {
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].email === email) {
+      return users[i];
+    }
+  }
+  return null;
+}
+
+// Função para exibir mensagens no elemento <p>
+function showMessage(message) {
+  document.getElementById('message').innerText = message;
+}
